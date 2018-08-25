ba67ace99d9e927bc66dea3ba2decb59603d07c7
sesap <nooooooooob@yandex.ru>
2018-08-25 20:40:13 +0300

    Another One

--- Arcade/Core/KillkthBit.py
+++ Arcade/Core/KillkthBit.py
@@ -1,25 +1,7 @@
-n = 37
+
+def killKthBit(n, k):
+    return int((format(n,'b')[:-k]+"0"+format(n,'b')[-k+1:]), 2)
+
+n = 2734827
 k = 4
-
-#k: 13
-#def killKthBit(n, k):
-#a = str(bin(n)[2:2+k])+'0'+str(bin(n)[3+k:])
-a = str(bin(n))
-a = a[2:]
-print(a)
-
-
-#b = str(bin(n)[2:-k:])+'0'+str(bin(n)[-k:-1:])
-
-#c = int(str(bin(n)[2:2+k-1])+str(0)+str(bin(n)[3+k-1:]),2)
-#c = int(str(bin(n)[2:2+k])+str(0)+str(bin(n)[3+k:]),2)
-#print(c)
-#print(b)
-#print(int(b,2))
-#c = str(bin(n))[2:]
-#print(c)
-#print(a)
-#print(a[-k])
-#print(int(a,2))
-
-#print(killKthBit(37, 3))
+print(killKthBit(n, k))
