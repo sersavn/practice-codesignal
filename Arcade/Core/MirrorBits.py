37c26cf68e2029a77779107843002af106041aa7
sesap <nooooooooob@yandex.ru>
2018-08-25 21:27:38 +0300

    Another One

---
+++ Arcade/Core/MirrorBits.py
@@ -0,0 +1,12 @@
+Example
+
+'''
+For a = 97, the output should be
+mirrorBits(a) = 67.
+
+97 equals to 1100001 in binary, which is 1000011 after mirroring,
+and that is 67 in base 10.
+'''
+
+def mirrorBits(a):
+    return int((format(a, 'b')[::-1]), 2)
